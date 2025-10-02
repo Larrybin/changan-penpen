ae2e61961b10f27499c85a9733a4e002c2d9ea92
Larrybin <odellaitkenasd00@gmail.com>
2025-10-03 02:37:22 +0800

    refactor(ui/button): default size uses tokenized padding via CSS variables (no visual change)

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -21,7 +21,8 @@ const buttonVariants = cva(
                 link: "text-primary underline-offset-4 hover:underline",
             },
             size: {
-                default: "h-9 px-4 py-2 has-[>svg]:px-3",
+                default:
+                    "h-9 px-[var(--button-px)] py-[var(--button-py)] has-[>svg]:px-[calc(var(--button-px)-0.25rem)]",
                 sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
                 lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
                 icon: "size-9",
