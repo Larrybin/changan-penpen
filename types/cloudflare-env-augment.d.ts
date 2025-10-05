066ae379e2faff589fa4bb7c2813e769bce74836
Larrybin <odellaitkenasd00@gmail.com>
2025-10-05 13:14:51 +0800

    fix(types): augment CloudflareEnv with admin config

---
+++ types/cloudflare-env-augment.d.ts
@@ -0,0 +1,10 @@
+import "@opennextjs/cloudflare/dist/api/cloudflare-context.js";
+
+declare global {
+    interface CloudflareEnv extends Cloudflare.Env {
+        ADMIN_ALLOWED_EMAILS: string;
+        ADMIN_ENTRY_TOKEN: string;
+    }
+}
+
+export {};
