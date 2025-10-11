ead06796298ba396283ecc412bbbaffbbdc8eb99
Larrybin <odellaitkenasd00@gmail.com>
2025-10-11 22:34:05 +0800

    chore: local push (code)

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,8 +4,8 @@ export const dynamic = "force-dynamic";
 
 export default async function Layout({
     children,
-}: {
+}: Readonly<{
     children: React.ReactNode;
-}) {
+}>) {
     return <DashboardLayout>{children}</DashboardLayout>;
 }
