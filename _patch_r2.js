792e7e3463111475dadf48b5c770004087b5d776
Larrybin <odellaitkenasd00@gmail.com>
2025-10-16 15:29:52 +0800

    chore: remove smart tool scripts and env/workflow manager
    remove scripts: smart-*, env manager, workflow optimizer; update package.json to drop related scripts

--- _patch_r2.js
+++
@@ -1,7 +0,0 @@
-﻿const fs = require('fs');
-const p = 'src/lib/r2.ts';
-let s = fs.readFileSync(p, 'utf8');
-s = s.replace(/maybeScanFile\(/g, '_maybeScanFile(');
-s = s.replace(/_scanFile,\s*\)/g, '_scanFile)');
-fs.writeFileSync(p, s);
-console.log('patched');
--- _rename_param.js
+++
@@ -1,6 +0,0 @@
-﻿const fs = require('fs');
-const p = 'src/lib/r2.ts';
-let s = fs.readFileSync(p, 'utf8');
-s = s.replace('function normalizeFolder(_folder: string | undefined)', 'function normalizeFolder(folder: string | undefined)');
-fs.writeFileSync(p, s);
-console.log('param renamed');
